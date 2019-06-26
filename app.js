5c8d6fc4df9797cb72ea252b73c7a0e8cdc73068
theclassicwizard <srkjadara@gmail.com>
2019-06-26 18:32:14 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,25 @@
+
+var request = require("request");
+var express= require("express");
+var app= express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extension: true}));
+app.use(express.static("public"));
+app.set("view engine", "ejs")
+app.listen(3000, function(){
+  console.log("Search Movies on Port 3000!!")
+});
+app.get("/results", function(req, res){
+  var query = req.query.search;
+  var url = "http://omdbapi.com/?apikey=e9d13928&s="+ query;
+  var img = "http://img.omdbapi.com/?apikey=e9d13928&s="+ query;
+  request(url, function(error, response, body){
+    if(!error && response.statusCode== 200){
+      var data = JSON.parse(body);
+      res.render("results", {data: data, movieSearch: query, imageUrl: img});
+    }
+});
+});
+app.get("/", function(req, res){
+  res.render("home");
+});
