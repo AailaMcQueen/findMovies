d17f4d01815edcdccce39243b477764be75a1f28
theclassicwizard <srkjadara@gmail.com>
2019-06-27 09:57:32 +0530

    Update

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var request = require("request");
 var express= require("express");
 var app= express();
 var bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extension: true}));
+app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs")
 app.listen(process.env.PORT||3000, function(){
